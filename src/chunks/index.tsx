e6cacd15a949506e7290fecfbf0b7896bf9d1172
eliasmurcray <eliasmurcray@gmail.com>
2023-12-18 16:10:10 -0800

    Update template

--- src/chunks/index.tsx
+++ src/chunks/index.tsx
@@ -1,17 +1,35 @@
-import "../css/about.css";
+import "../css/index.css";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
-class App extends React.Component {
-  constructor() {
-    super(null);
+type AppState = {
+  clicks: number;
+};
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      clicks: 0,
+    };
+  }
+
+  handleClick(event: React.MouseEvent<HTMLButtonElement>): void {
+    this.setState((state: Readonly<AppState>) => ({
+      clicks: state.clicks + 1,
+    }));
   }
 
   render(): React.JSX.Element {
-    return <p>Hello World!</p>;
+    return (
+      <React.Fragment>
+        {this.state.clicks} <button onClick={this.handleClick}>Click Me</button>
+      </React.Fragment>
+    );
   }
 }
 
-const root = ReactDOM.createRoot(document.body);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
