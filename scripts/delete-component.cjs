8bdc77563bc65071e08721d3fa0075ef0a2961a0
eliasmurcray <eliasmurcray@gmail.com>
2023-12-19 22:16:11 -0800

    Fix edge case no component name

--- scripts/delete-component.cjs
+++ scripts/delete-component.cjs
@@ -2,6 +2,11 @@ const fs = require("node:fs");
 
 const componentName = process.argv[3];
 
+if (componentName === undefined || componentName === null) {
+        console.error("Error: Must provide a component  name");
+        process.exit(1);
+}
+
 const deleteFile = (path) => {
 	if (fs.existsSync(path)) {
 		fs.unlinkSync(path);
